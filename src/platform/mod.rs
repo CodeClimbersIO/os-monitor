adc7970c958efdbfc6e5a3f69fdcca9e9554ef7d
Arthur Wallendorff <howdy@wallenart.dev>
2025-09-13 01:02:05 +0200

    chore: add linux build target

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -10,6 +10,11 @@ mod windows;
 #[cfg(target_os = "windows")]
 pub(crate) use windows::*;
 
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "linux")]
+pub(crate) use linux::*;
+
 use crate::{BlockableItem, Monitor, MonitorError};
 
 pub fn start_monitoring(monitor: Arc<Monitor>) {
